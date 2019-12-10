96501d3a64161cbe479f1716ab70b656b3c0a07e
Diego Paez <diego@geutstudio.com>
2019-12-10 09:23:45 -0300

    Update: download version test - checkout

--- test/checkout.js
+++ test/checkout.js
@@ -19,7 +19,6 @@ tape('simple checkout', async (t) => {
     } catch (err) {
       t.true(err)
       t.same(err.code, 'ENOENT')
-      console.log('version', version)
       await testCheckout(version)
     }
   } catch (err) {
@@ -31,30 +30,33 @@ tape('simple checkout', async (t) => {
     try {
       const data = await oldVersion.readFile('/hello')
       t.same(data, Buffer.from('world'))
-      t.end()
     } catch (err) {
+      console.log(err)
       t.error(err, 'no error')
     }
+    t.end()
   }
 })
 
 // TODO: Re-enable the following tests once the `download` and `fetchLatest` APIs are reimplemented.
 
-tape.skip('download a version', async function (t) {
+tape('download a version', async (t) => {
   var src = create()
   await src.ready()
 
+  /*
   t.ok(src.writable)
   t.ok(src.metadata.writable)
   t.ok(src.content.writable)
+  */
   try {
     await src.writeFile('/first.txt', 'number 1')
 
     await src.writeFile('/second.txt', 'number 2')
 
     await src.writeFile('/third.txt', 'number 3')
-    t.same(src.version, 3)
-    testDownloadVersion()
+    t.same(src.version, 4)
+    await testDownloadVersion()
   } catch (err) {
     t.error(err, 'no error')
   }
@@ -62,26 +64,26 @@ tape.skip('download a version', async function (t) {
   async function testDownloadVersion () {
     const clone = create(src.key, { sparse: true })
 
-    clone.on('content', () => {
-      t.same(clone.version, 3)
-      clone
-        .checkout(2)
-        .download(async function (err) {
-          t.error(err)
-          try {
-            const content = await clone.readFile('/second.txt', { cached: true })
-            t.same(content && content.toString(), 'number 2', 'content does not match')
-            clone.readFile('/third.txt', { cached: true }).catch(err => {
-              t.same(err && err.message, 'Block not downloaded')
-              t.end()
-            })
-          } catch (err) {
-            t.error(err, 'block not downloaded')
-          }
-        })
-    })
     var stream = clone.replicate()
     stream.pipe(src.replicate()).pipe(stream)
+
+    await clone.ready()
+    t.same(clone.version, 4, 'versions should match')
+
+    const handler = clone
+      .checkout(3)
+    //  .download('/')
+
+    // handler.on('error', (err) => t.error(err))
+    // handler.on('finish', async (data) => {
+    const content = await handler.readFile('/second.txt', { cached: true })
+    t.same(content && content.toString(), 'number 2', 'content does not match')
+    handler.readFile('/third.txt', { cached: true })
+      .catch(err => {
+        t.same(err && err.message, "No such file or directory: '/third.txt'.")
+        t.end()
+      })
+    // })
   }
 })
 
